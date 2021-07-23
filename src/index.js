17fe6a44825ebaee0037a4e9c3944a96d62a0263
Jesse <85474982+Jesse0502@users.noreply.github.com>
2021-07-23 16:08:56 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+  import { frontPage } from "./frontPage/frontPage"
+  import menu from '../src/menu/menu'
+  import aboutUs from '../src/aboutus/aboutus'
+  console.log("welcome")
+  frontPage();
+  // menu();
+  // aboutUs();
